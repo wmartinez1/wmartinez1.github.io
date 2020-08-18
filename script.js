a05db4e027d98aed1cac77360b16af4254d51d78
Wellington Martinez <wellingtonmartinezxx@aol.com>
2020-08-18 19:28:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//hover over image code
+function changePicture(pictureNumber){
+    var image= document.getElementById("myImage");
+    if(pictureNumber===1){
+  image.src= "photo1.jpg"
+  }else{
+    image.src="photo2.jpg"
+    }
+  }
